92071cdf6cf01338674cf6ed5cd435e8b43f597d
Housey <housebreaker1@gmail.com>
2017-11-19 22:20:40 +0200

    refactor partial table view

--- BiraIssueTrackerCore.Services/IssueService.cs
+++ BiraIssueTrackerCore.Services/IssueService.cs
@@ -3,21 +3,26 @@ using System.Linq;
 using BiraIssueTrackerCore.Data;
 using BiraIssueTrackerCore.Data.Models;
 using BiraIssueTrackerCore.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace BiraIssueTrackerCore.Services
 {
-    public class IssueService : IIssueService
-    {
-	    private readonly IssueTrackerDbContext context;
+	public class IssueService : IIssueService
+	{
+		private readonly IssueTrackerDbContext context;
 
-	    public IssueService(IssueTrackerDbContext context)
-	    {
-		    this.context = context;
-	    }
+		public IssueService(IssueTrackerDbContext context)
+		{
+			this.context = context;
+		}
 
-	    public IEnumerable<Issue> GetAllIssues()
-	    {
-		    return this.context.Issues.ToArray();
-	    }
-    }
+		public IEnumerable<Issue> GetAllIssues() =>
+			this.context
+			.Issues
+			.Include(i => i.Author)
+			.Include(i => i.Assignee)
+			.Include(i => i.IssueTags)
+				.ThenInclude(it => it.Tag)
+			.ToArray();
+	}
 }
