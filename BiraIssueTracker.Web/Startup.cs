aa5804de50b036fbfc0828ef7f6416c789be4ef9
Housey <housebreaker1@gmail.com>
2017-11-19 21:23:04 +0200

    remove unused "using" statements

--- BiraIssueTracker.Web/Startup.cs
+++ BiraIssueTracker.Web/Startup.cs
@@ -5,7 +5,6 @@ using BiraIssueTrackerCore.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
